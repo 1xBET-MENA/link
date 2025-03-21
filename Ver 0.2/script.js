ed8fdc22d4aad751f7ca0260f21401eb60bb5eb4
1xBET-MENA <mokhamad.r@mailbox366.com>
2025-03-21 19:57:32 +0300

    Update script.js

--- Ver 0.2/script.js
+++ Ver 0.2/script.js
@@ -1,36 +1,44 @@
 const emailLinks = {
-    "egypt": "mailto:processing@eg.1xbet-team.com",
-    "morocco": "mailto:processing-morocco@1xbet-team.com",
-    "mauritania": "mailto:processing-iq@1xbet-team.com",
-    "iraq": "mailto:processing-iq@1xbet-team.com",
-    "jordan": "mailto:jordan@example.com",
-    "algeria": "mailto:processing-algeria@1xbet-team.com",
-    "tunisia": "mailto:processing-tunisia@1xbet-team.com",
-    "djibouti": "mailto:processing-djibouti@1xbet-team.com",
-    "other-arab": "mailto:processing-ar@1xbet-team.com"
+    egypt: "mailto:processing@eg.1xbet-team.com",
+    morocco: "mailto:processing-morocco@1xbet-team.com",
+    mauritania: "mailto:processing-iq@1xbet-team.com",
+    iraq: "mailto:processing-iq@1xbet-team.com",
+    jordan: "mailto:jordan@example.com",
+    algeria: "mailto:processing-algeria@1xbet-team.com",
+    tunisia: "mailto:processing-tunisia@1xbet-team.com",
+    djibouti: "mailto:processing-djibouti@1xbet-team.com",
+    other: "mailto:processing-ar@1xbet-team.com"
 };
+
 const telegramLinks = {
-    "iraq": "https://t.me/xBET_MENA_IRQ",
-    "jordan": "https://t.me/xBET_MENA_JOR",
+    iraq: "https://t.me/xBET_MENA_IRQ",
+    jordan: "https://t.me/xBET_MENA_JOR"
 };
 
 let isEmailMode = true;
 
 const switchButton = document.getElementById("switchBtn");
 const switchIcon = document.getElementById("switchIcon");
+const switchText = document.getElementById("switchText");
+const linksContainer = document.getElementById("linksContainer");
+
+const updateLinks = () => {
+    const links = isEmailMode ? emailLinks : telegramLinks;
+    linksContainer.innerHTML = "";
+
+    for (let country in links) {
+        const newLink = document.createElement("a");
+        newLink.href = links[country];
+        newLink.innerText = `1xBET ${country.toUpperCase()} - بالعربي`;
+        linksContainer.appendChild(newLink);
+    }
+};
 
 switchButton.addEventListener("click", () => {
     isEmailMode = !isEmailMode;
-    
-    if (isEmailMode) {
-        switchIcon.classList.remove("fa-paper-plane");
-        switchIcon.classList.add("fa-envelope");
-    } else {
-        switchIcon.classList.remove("fa-envelope");
-        switchIcon.classList.add("fa-paper-plane");
-    }
-
-    let country = "iraq";
-    let link = isEmailMode ? emailLinks[country] || emailLinks["default"] : telegramLinks[country] || telegramLinks["default"];
-    window.location.href = link;
+    switchIcon.className = isEmailMode ? "fas fa-envelope" : "fas fa-paper-plane";
+    switchText.textContent = isEmailMode ? "راسلنا مباشرة عبر البريد الإلكتروني" : "راسلنا عبر بوت تيليجرام";
+    updateLinks();
 });
+
+updateLinks();
