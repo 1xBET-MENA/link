a086f8ceb055d5bc3224ef240faa168b9866bc63
1xBET-MENA <mokhamad.r@mailbox366.com>
2025-03-21 17:10:51 +0300

    Create script.js

---
+++ Ver 0.2/script.js
@@ -0,0 +1,38 @@
+const emailLinks = {
+    "egypt": "mailto:processing@eg.1xbet-team.com",
+    "morocco": "mailto:processing-morocco@1xbet-team.com",
+    "mauritania": "mailto:processing-iq@1xbet-team.com",
+    "iraq": "mailto:processing-iq@1xbet-team.com",
+    "jordan": "mailto:jordan@example.com",
+    "algeria": "mailto:processing-algeria@1xbet-team.com",
+    "tunisia": "mailto:processing-tunisia@1xbet-team.com",
+    "djibouti": "mailto:processing-djibouti@1xbet-team.com",
+    "djibouti": "mailto:processing-djibouti@1xbet-team.com",
+    "default": "mailto:processing-ar@1xbet-team.com"
+};
+
+const telegramLinks = {
+    "iraq": "https://t.me/iraqpaymentssupport_bot",
+    "jordan": "https://t.me/jordanpaymentssupport_bot",
+};
+
+let isEmailMode = true;
+
+const switchButton = document.getElementById("switchBtn");
+const switchIcon = document.getElementById("switchIcon");
+
+switchButton.addEventListener("click", () => {
+    isEmailMode = !isEmailMode;
+
+    if (isEmailMode) {
+        switchIcon.classList.remove("fa-paper-plane");
+        switchIcon.classList.add("fa-envelope");
+    } else {
+        switchIcon.classList.remove("fa-envelope");
+        switchIcon.classList.add("fa-paper-plane");
+    }
+
+    let country = "egypt";
+    let link = isEmailMode ? emailLinks[country] || emailLinks["default"] : telegramLinks[country] || telegramLinks["default"];
+    window.location.href = link;
+});
